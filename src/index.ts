e1ec0c6319cfaa42039d7e7219cb4231e26e6ec0
Rakengot-v2 <danil.yadlos@gmail.com>
2025-09-30 17:46:17 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let userName: string = "Ibragim";
+let userAge: number = 27;
+let isStudent: boolean = true;
+
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+console.log(`Student: ${isStudent}`);
