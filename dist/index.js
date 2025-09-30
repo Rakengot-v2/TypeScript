7351bb25466e7a2b34a6cf95e512b979d921aa22
Rakengot-v2 <danil.yadlos@gmail.com>
2025-09-30 17:48:58 +0300

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "Ibragim";
+let userAge = 27;
+let isStudent = true;
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+console.log(`Student: ${isStudent}`);
